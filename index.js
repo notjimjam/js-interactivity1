86ef1325e3f3665eabd46b1743799e580e1988fa
Jamie Smith <thejamiemsmith@gmail.com>
2022-02-23 14:46:48 -0700

    complete through step 3

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ function addMovie (event) {
     const movieTitle = document.createElement('span')
     movieTitle.textContent = inputField.value
     movie.appendChild(movieTitle)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
 
     const list = document.querySelector('ul')
     list.appendChild(movie)
     
-    inputField.value = ''
-    
+    inputField.value = ''    
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
 }
 
 document.querySelector('form').addEventListener('submit', addMovie)
